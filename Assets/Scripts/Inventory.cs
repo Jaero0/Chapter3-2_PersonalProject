efe183dbbf283bdabd51850f39505a2c22f77f27
Jaero0 <wjdwogh1224@gmail.com>
2023-09-20 21:24:32 +0900

    Fixing
    아이템 슬롯에 알맞게 아이템 추가되도록 함...
    
    버그
    아이템스크롤하면 슬록은 사라지고, 아이템은 둥둥 뜸
    아이템 스케일이 굉장히 작아짐.. 아마 좌표고정 UI로 움직이면서 생기는 문제라고 생각이 됨

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,33 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class Inventory : MonoBehaviour
 {
     public GameObject invenSlot;
     public GameObject scrollViewContent;
 
+    [SerializeField] List<GameObject> totalItem = new List<GameObject>(); //scriptableObj list ini
+
     [SerializeField] private TextMeshProUGUI itemTotalCountTxt;
 
-    Dictionary<GameObject, string> inventorySlot_ItemName = new Dictionary<GameObject, string>();
+    Dictionary<string, string> inventorySlot_ItemName = new Dictionary<string, string>();
 
     void Awake()
     {
-        InstanciateItemSlot();
+        IncreaseItemScale();
+        InstanciateItem_Slot();
         DisplayInvenInfo();
     }
 
-    void InstanciateItemSlot()
+    void IncreaseItemScale() //아이템크기가 왜인진 모르게 작음...
+    {
+        for (int i = 0; i < totalItem.Count; i++)
+        {
+            totalItem[i].transform.localScale = new Vector3(70, 70, 1);
+        }
+    }
+
+    void InstanciateItem_Slot() //TODO : 아이템 스크롤하면 아이템만 둥둥 뜸
     {
         for (int i = 0; i < 30; i++)
         {
             GameObject slot;
             slot = Instantiate(invenSlot, scrollViewContent.transform);
-            inventorySlot_ItemName.Add(slot, null);
+            if (i < totalItem.Count)
+            {
+                var items = Instantiate(totalItem[i], slot.transform);
+                inventorySlot_ItemName.Add("slot " + i, totalItem[i].name);
+            }
+            else
+            {
+                inventorySlot_ItemName.Add("slot " + i, null);
+            }
+        }
+
+        foreach (KeyValuePair<string, string> item in inventorySlot_ItemName)
+        {
+            Debug.Log($"({item.Key} : {item.Value})");
         }
     }
 
-    void DisplayInvenInfo()
+    void DisplayInvenInfo() //TODO : 현재 아이템 갯수 표시하기
     {
         itemTotalCountTxt.text = " / " + inventorySlot_ItemName.Count.ToString();
     }
